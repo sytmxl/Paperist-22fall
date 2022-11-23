80f70d91b0222bc1335a807d911108741f74d375
MOYFbeta <1415291531@qq.com>
2022-11-23 20:43:30 +0800

    完成了搜索栏

---
+++ front_end/src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-row  style="min-height: calc(8vh); background: #B3C0D1">
+    <el-col :span="4"  style="margin-right: calc(1vw); cursor:pointer">
+      <img src="../assets/logo/bar_name_logo.svg" style="height: calc(4vw)" @click="gotoFirstPage">
+    </el-col>
+    <el-col :span="14">
+      <search-box style="margin-top:calc(1.5vh);max-width: calc(40vw)"/>
+    </el-col>
+    <el-col :span="5" style="margin-top: calc(0.5vw); margin-right: calc(1vh)">
+      <div @click="gotoPersonalInformation">
+        <el-avatar :src="require('../assets/mope.jpeg')"
+                   style="float: right; margin-right: calc(1vw); margin-top: calc(0.5vh); cursor:pointer"
+        ></el-avatar>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import SearchBox from "@/components/SearchBox";
+export default {
+  name: "SearchBar",
+  components: {SearchBox},
+  methods:{
+    gotoFirstPage(){
+      location.href = "/FirstPage";
+    },
+    gotoPersonalInformation(){
+      location.href = "/PersonalInformation";
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
