5c6405adab76859377d39ad805deaff179ad2652
MOYFbeta <1415291531@qq.com>
2022-11-24 01:09:24 +0800

    改了下搜索结果的排版

---
+++ front_end/src/components/PaperCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-card shadow="hover">
+    <h3 style="text-align: left">{{$props.title}}</h3>
+    <p style="text-align: left; margin-top: 1%; margin-right: 1%">{{limitWords(content)}}</p>
+
+    <div style="margin-top: 2%; float: left">
+      <p style=" text-align: left;color: #B3C0D1;display: inline;">作者:</p>
+      <p style=" text-align: left;display: inline;">{{getAuthorsList()}}</p>
+    </div>
+
+    <div style="margin-top: 2%; float: left">
+      <p style=" text-align: left;color: #B3C0D1;display: inline;">来源:</p>
+      <p style=" text-align: left;display: inline;">{{source}}</p>
+    </div>
+
+    <el-button-group style="float: right; margin-bottom: 2%">
+      <el-button @click = clickFavourite>
+        <i v-if="isFavourite === false" class="el-icon-star-off"></i>
+        <i v-else class="el-icon-star-on"></i>
+      </el-button>
+      <el-button @click = clickFavourite>
+        <i class="el-icon-paperclip"></i>
+      </el-button>
+    </el-button-group>
+  </el-card>
+</template>
+
+<script>
+import {$data} from "../../static/pdf/build/pdf.worker";
+
+export default {
+  name: "PaperCard",
+  props: {
+    title: {default :"关于ZCH费了半天劲也不知道该在这填点啥的研究"},
+    authors: {default:["ZCH233","ZCH234","ZCH235"]},
+    content: {default: " 这可真是太迷惑了，你说是不是呢。今天跟着小编一起看看为什么德国和阿根廷会在世界杯上输给沙特和日本吧。这可真是太迷惑了，你说是不是呢。今天跟着小编一起看看为什么德国和阿根廷会在世界杯上输给沙特和日本吧。这可真是太迷惑了，你说是不是呢。今天跟着小编一起看看为什么德国和阿根廷会在世界杯上输给沙特和日本吧。这可真是太迷惑了，你说是不是呢。今天跟着小编一起看看为什么德国和阿根廷会在世界杯上输给沙特和日本吧。这可真是太迷惑了，你说是不是呢。今天跟着小编一起看看为什么德国和阿根廷会在世界杯上输给沙特和日本吧。"},
+    source:{default:"自动废话集"}
+  },
+  data(){
+    return{
+      isFavourite : false
+    }
+  },
+  methods:{
+    limitWords(txt){
+      let str = txt;
+      str = str.substr(0,100) + '...';
+      return str;
+    },
+    clickFavourite(){
+      this.isFavourite = !this.isFavourite;
+    },
+    getAuthorsList(){
+      let str = "";
+      let len = this.authors.length-1
+      for(let i = 0;i < len; i++){
+        str = str + this.authors[i] + ",";
+      }
+      return str + this.authors[this.authors.length-1]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- front_end/src/views/User/SearchInformation.vue
+++ front_end/src/views/User/SearchInformation.vue
@@ -1,22 +1,21 @@
 <template>
-  <el-container id="SearchInformation">
+  <el-container id="SearchInformation" class="light-mode">
 <!--    顶栏-->
-    <el-header height="calc(10vh)">
+    <el-header>
     <search-bar/>
     </el-header>
-    <el-container>
+    <el-container style="width: calc(80vw);align-self: center">
 <!--      左侧栏-->
-      <el-aside >
-        <h3 style="text-align: left; margin-left: 10%;">二次搜索筛选</h3>
+      <el-aside style="max-width: calc(20vw); margin-top: calc(5vh)">
         <div>
-          <el-card style="background: #B3C0D1; margin-bottom: 10px">
+          <el-card style="margin-bottom: 10px" class="display_zone" shadow="never">
             <!--        复选框组1-->
             <el-checkbox-group  v-model="secondarySearchFilters01" size="mini">
               <el-checkbox border v-for="filter in filterGroup01" :label="filter" :key="filter"
                            style="background: white; margin: 1% ;float: left"/>
             </el-checkbox-group>
           </el-card>
-          <el-card style="background: #B3C0D1; margin-bottom: 10px">
+          <el-card style="margin-bottom: 10px" class="display_zone">
             <!--        复选框组2-->
             <el-checkbox-group v-model="secondarySearchFilters02" size="mini">
               <el-checkbox border v-for="filter in filterGroup02" :label="filter" :key="filter"
@@ -28,23 +27,38 @@
       </el-aside>
 <!--      搜索结果-->
       <el-main>
-        <h3 >搜索结果</h3>
+        <el-row :gutter="20">
+          <el-col :span="16" style="text-align: left; margin-left: 6%; margin-bottom: 3%; color: #B3C0D1">找到约{{toThousands(resultNum)}}条相关结果</el-col>
+          <el-col :span="4" style="text-align: left; margin-left: 6%; margin-bottom: 3%; color: #B3C0D1">
+            <el-dropdown style="display: inline-block">
+              <span>
+                <span style="cursor: pointer;color: #B3C0D1">
+                  {{sortMethod}}
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>默认</el-dropdown-item>
+                  <el-dropdown-item>引用量</el-dropdown-item>
+                  <el-dropdown-item>日期</el-dropdown-item>
+                </el-dropdown-menu>
+              </span>
+            </el-dropdown>
+            <i style="display: inline-block; margin-left: 10%" class="el-icon-sort" @click="sortReserve"></i>
+          </el-col>
+        </el-row>
+
         <div>
-          <el-card style="background: #B3C0D1; min-height: calc(75vh)">
-            <el-card v-for="searchResult in searchResults" :key="searchResult" v-loading = "true"
-                     style = "height: 150px;margin-bottom: 10px">
-              <!-- TODO 之后需要复用搜索结果的卡片-->
-              <h5 style="margin-left: 10%; text-align: left;">{{searchResult}}</h5>
-              <search-item></search-item>
-            </el-card>
+          <el-card style="min-height: calc(75vh)" class="display_zone" shadow="never">
+            <paper-card v-for="searchResult in searchResults"
+                        style="margin-top: calc(1vh)"
+            />
           </el-card>
         </div>
       </el-main>
 <!--右侧栏-->
-      <el-aside>
-        <h3 style="text-align: right; margin-left: 10%">推荐</h3>
-        <el-card style="background: #B3C0D1">
-          <el-card v-for="recommend in recommends" :key="recommend" v-loading = "true"
+      <el-aside style="max-width: calc(20vw); margin-top: calc(5vh)">
+        <el-card class="display_zone" shadow="never">
+          <h3 style="text-align: left; margin-left: 5%; margin-bottom: calc(2vh)">推荐</h3>
+          <el-card v-for="recommend in recommends" :key="recommend" v-loading = "true" shadow="never"
                    style = "height: 75px;margin-bottom: 10px">
             <h5 style="margin-left: 10%; text-align: left;">{{recommend}}</h5>
           </el-card>
@@ -59,9 +73,12 @@
 <script>
 
 import SearchBar from "@/components/SearchBar";
+import PaperCard from "@/components/PaperCard";
+import PaperInformation from "@/views/User/PaperInformation";
+import {$data} from "../../../static/pdf/build/pdf.worker";
 export default {
   name:"SearchInformation",
-  components: {SearchBar},
+  components: {PaperInformation, PaperCard, SearchBar},
   props :{
   },
   data() {
@@ -76,10 +93,24 @@ export default {
       //搜索结果，暂时留白
       searchResults : ['结果A','结果B','结果A','结果B','结果A','结果B','结果A','结果B','结果A','结果B','结果A','结果B',],
       //推荐栏，暂时留白
-      recommends : ['随便','在这里','推荐点什么','a','b',]
+      recommends : ['随便','在这里','推荐点什么','a','b',],
+      resultNum : 2333333,
+      sortMethod : "默认",
+      sortReserve : false
     }
   },
   methods :{
+    toThousands(num) {
+      var result = [ ], counter = 0;
+      num = (num || 0).toString().split('');
+      for (var i = num.length - 1; i >= 0; i--) {
+        counter++;
+        result.unshift(num[i]);
+        if (!(counter % 3) && i != 0) { result.unshift(','); }
+      }
+      return result.join('');
+    },
+    reserveSort(){$data.sortReserve = !$data.sortReserve;}
 
   }
 }
