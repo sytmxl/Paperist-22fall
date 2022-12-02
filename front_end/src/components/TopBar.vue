a8665edda03604688cd79b522121df328f5bd65b
sytmxl <1728999839@qq.com>
2022-12-02 12:45:33 +0800

    pull

--- front_end/src/components/TopBar.vue
+++ front_end/src/components/TopBar.vue
@@ -11,22 +11,6 @@
       </div>
     </div>
   </el-header>
-  <!-- <el-row class="header_show"
-      style="height: calc(8vh)">
-    <el-col :offset="3" :span="4"  style= "margin-right: calc(1vw); cursor:pointer">
-      <img src="../assets/logo_for_dark/bar_name_logo.svg" style="height: calc(4vw)" @click="gotoFirstPage">
-    </el-col>
-    <el-col :span="10">
-      <search-box style="margin-top:calc(1vh);  width: calc(50vw); float: left"/>
-    </el-col>
-    <el-col :span="5" style="margin-top: calc(0.5vw); margin-right: calc(1vh)">
-      <div @click="gotoPersonalInformation">
-        <el-avatar :src="require('../assets/mope.jpeg')"
-                   style="float: right; margin-top: calc(0.5vh); cursor:pointer"
-        ></el-avatar>
-      </div>
-    </el-col>
-  </el-row> -->
 </template>
 
 <script>
@@ -94,23 +78,22 @@ export default {
 
 <style lang="scss" scoped>
 #topbar {
-  
   z-index: 100;
   width: 100%;
   height:60px;
   position: fixed; 
   transition: 0.3s;
-  // animation-name: exit_blur;
-  // animation-iteration-count: 1;
-  // animation-duration: 0.4s;
   animation: test 0.3s cubic-bezier(.23,1.38,.65,.99);
 }
-
 @keyframes test {
   0% {
+    filter: blur(20px);
+    opacity: 0%;
     top:-60px;
   }
   100% {
+    filter: blur(0px);
+    opacity: 100%;
     top:0px;
   }
 }
@@ -122,63 +105,61 @@ export default {
   display: flex;
   border-radius: 0px 0px 20px 20px ;
 }
+.dark-mode {
+  #bar-content {
+    // background-color: #fff !important;
+  }
+}
 .logo {
   // background: rgb(255, 255, 255);
   float: left;
   transition: 0.3s;
+  &:hover {
+    cursor: pointer;
+  }
 }
-.logo:hover {
-  cursor: pointer;
-  // margin: 200px;
-}
-
 .search_input {
   margin: 10px auto 0px auto;
-  width: 300px;
+  width: 500px;
   transition: 0.3s;
   padding: auto;
   // overflow: hidden;
-  &:hover {
-    width: 800px;
-    /deep/.el-input-group__prepend {
-      display: table-cell;
-    }
-    /deep/.advsearch {
-      display: block;
-    }
-  }
+
 }
+.el-input {
+  width: 80px !important;
+}
+.search_input:focus{
+    background-color: #bddce9 !important;
+  // width: 800px;
+    
+    // /deep/.el-input-group__prepend {
+    //   display: table-cell;
+    // }
+    // /deep/.advsearch {
+    //   display: block;
+    // }
+  }
 /deep/.el-input-group__prepend {
-  display: none;
+  // display: none;
   transition: 0.3s;
 }
 /deep/.advsearch {
-  display: none;
+  // display: none;
   transition: 0.8s;
 }
-// .content:hover {
-//   .search_input {
-//     width: 800px;
-//     /deep/.el-input-group__prepend {
-//       display: table-cell;
-//     }
-//     /deep/.advsearch {
-//       display: block;
-//     }
-//   }
-// }
+
 #switch {
   width: 100px;
   height: 40px;
-  margin: 10px 20px 0px 0px;
+  margin: 10px 20px 0px 50px;
   border: 1px solid white;
-
 }
 .el-avatar {
   margin: 10px 10px 0px 0px;
-}
-.el-avatar:hover {
-  cursor: pointer;
+  &:hover {
+    cursor: pointer;
+  }
 }
 @keyframes exit_blur {
   0% {
