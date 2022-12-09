75a7e48fc7c73234024e9e6b4b87c1c92f6ba303
HuBoXuan <1450858017@qq.com>
2022-12-09 13:58:50 +0800

    添加剪切板功能，需要npm install

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -6,9 +6,11 @@ import ElementUI from 'element-ui'
 import '../src/assets/icon/iconfont.css'
 import 'element-ui/lib/theme-chalk/index.css';
 import instance from "@/http";
+import VueClipboard from 'vue-clipboard2'
 Vue.prototype.$axios = instance
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+Vue.use(VueClipboard)
 
 new Vue({
   router,
