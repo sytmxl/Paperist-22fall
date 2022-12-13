ad45e43732b969abc03ae79b41ee19601e1f4388
MOYFbeta <1415291531@qq.com>
2022-12-13 19:22:08 +0800

    尝试进行部署

--- front_end/src/http.js
+++ front_end/src/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 axios.defaults.baseURL = ""
 axios.defaults.headers ["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
-let instance = axios.create({timeout: 1000 * 12});
+let instance = axios.create({timeout: 1000 * 12,baseURL:"http://124.70.8.57/"});
 instance.interceptors.request.use(
     config => {
         if (sessionStorage.getItem("token")) {
